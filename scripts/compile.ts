8f806b22d44ac4d84eafc5667fa04859ea4a8269
deLordeee <evgeniy.one.2013@gmail.com>
2024-04-19 21:49:14 +0300

    sus

---
+++ scripts/compile.ts
@@ -0,0 +1,43 @@
+import * as fs from "fs";
+import process from "process";
+import { Cell } from "ton-core";
+import { compileFunc } from "@ton-community/func-js";
+import { buffer } from "stream/consumers";
+
+
+
+async function compileScript(){
+    console.log(
+        "====================================================================="
+    );
+    console.log(
+        "Compile script is running , lets find some FunC code to compile..."
+    );
+
+    const compileResult = await compileFunc({
+        targets:[".contracts/main.fc"],
+        sources:(x)=>fs.readFileSync(x).toString("utf8")
+
+    });
+
+    if(compileResult.status === "error"){
+        console.log("Error!:");
+        console.log('\n${compileResult.message}');
+        process.exit(1);
+    }
+
+    const hexArtifact = 'build/main.compiled.json';
+
+    fs.writeFileSync(
+        hexArtifact,
+        JSON.stringify({
+            hex:Cell.fromBoc(Buffer.from(compileResult.codeBoc,"base64"))[0]
+                .toBoc()
+                .toString("hex"),
+
+        })
+
+    );
+    console.log("Code saved to" + hexArtifact);
+}
+compileScript()
